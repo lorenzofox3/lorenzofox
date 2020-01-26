26671e74745a0e49482542afa5b1346e123af001
Laurent RENARD <lorenzofox@Laurents-MacBook-Pro.local>
2020-01-26 14:46:10 +0100

    save work

--- pages/index.js
+++ pages/index.js
@@ -1,143 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
+import React from 'react';
 import Head from '../components/head';
 import Nav from '../components/nav';
 
 const Home = () => {
-  const [date, setDate] = useState(null);
-
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.json();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-
-  return (
-    <div>
-      <Head title="Home" />
-      <Nav />
-
-      <div className="hero">
-        <h1 className="title">Welcome to Next!</h1>
-        <p className="description">
-          To get started, edit the <code>pages/index.js</code> or{' '}
-          <code>pages/api/date.js</code> files, then save to reload.
-        </p>
-
-        <p className="row date">
-          The date is:&nbsp;{' '}
-          {date ? (
-            <span>
-              <b>{date.date}</b>
-            </span>
-          ) : (
-            <span className="loading"></span>
-          )}
-        </p>
-
-        <div className="row">
-          <Link href="https://github.com/zeit/next.js#setup">
-            <a className="card">
-              <h3>Getting Started &rarr;</h3>
-              <p>Learn more about Next.js on GitHub and in their examples.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js/tree/master/examples">
-            <a className="card">
-              <h3>Examples &rarr;</h3>
-              <p>Find other example boilerplates on the Next.js GitHub.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js">
-            <a className="card">
-              <h3>Create Next App &rarr;</h3>
-              <p>Was this tool helpful? Let us know how we can improve it!</p>
-            </a>
-          </Link>
-        </div>
-      </div>
-
-      <style jsx>{`
-        .hero {
-          width: 100%;
-          color: #333;
-        }
-        .title {
-          margin: 0;
-          width: 100%;
-          padding-top: 80px;
-          line-height: 1.15;
-          font-size: 48px;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .row {
-          max-width: 880px;
-          margin: 80px auto 40px;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-around;
-        }
-        .date {
-          height: 24px;
-          max-width: calc(100% - 32px)
-          text-align: center;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          padding: 0 16px;
-        }
-        .date p {
-          text-align: center;
-        }
-        .date span {
-          width: 176px;
-          text-align: center;
-        }
-        @keyframes Loading {
-          0%{background-position:0% 50%}
-          50%{background-position:100% 50%}
-          100%{background-position:0% 50%}
-        }
-        .date .loading {
-          max-width: 100%;
-          height: 24px;
-          border-radius: 4px;
-          display: inline-block;
-          background: linear-gradient(270deg, #D1D1D1, #EAEAEA);
-          background-size: 200% 200%;
-          animation: Loading 2s ease infinite;
-        }
-        .card {
-          padding: 18px 18px 24px;
-          width: 220px;
-          text-align: left;
-          text-decoration: none;
-          color: #434343;
-          border: 1px solid #9b9b9b;
-        }
-        .card:hover {
-          border-color: #067df7;
-        }
-        .card h3 {
-          margin: 0;
-          color: #067df7;
-          font-size: 18px;
-        }
-        .card p {
-          margin: 0;
-          padding: 12px 0 0;
-          font-size: 13px;
-          color: #333;
-        }
-      `}</style>
-    </div>
-  );
+    return (
+        <div>
+            <Head title="About"/>
+            <Nav/>
+            <main>
+                <h1>About</h1>
+                <p>
+                    Mad about code, I have the luck to live from my passion ! Life has led me to spend one year
+                    in <strong>Malaysia</strong>, two in <strong>Vietnam</strong> and four in <strong>Cuba</strong>.
+                    Every time the circumstances were different in such a way
+                    I could work for big companies, with startup or smaller non profit structures. It has been within a
+                    team, remotely or alone; in French, English or Spanish. You will have understood:
+                    I <strong>adapt</strong>.
+                </p>
+                <p>
+                    I love to create the architecture of a system and find <strong>simple solutions to complex
+                    problems</strong>. I will
+                    prefer an adapted solution based on the needs and the constraints over the trend of a technology.
+                </p>
+                <p>
+                    I believe the job goes beyond few lines of code dropped in a text file and must be part of an
+                    automated process
+                    where <strong>robustness</strong>, <strong>test-ability</strong> and <strong>extensibility</strong> are
+                    as important as the source
+                    code itself.
+                </p>
+                <p>
+                    This job is very demanding, constantly changing and asks at every moment a great dose
+                    of <strong>curiosity</strong>,
+                    <strong>humility</strong> and <strong>empathy</strong>. Instead of frightening me, I see these
+                    requirements as an opportunity to grow
+                    and learn.
+                </p>
+            </main>
+        </div>);
 };
 
 export default Home;
